1539ef52affb4647a3639fa9fc2acba82a73687a
drat3 <drat3@protonmail.ch>
2023-12-23 22:46:24 +0100

    set up guvicorn

--- services/api/main.py
+++ services/api/main.py
@@ -1,8 +1,13 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-def root():
-    return {"Hello": "World"}
+async def hello():
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)

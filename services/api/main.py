08908203aad0e9754e2ea4c1cd2e3702c049c3ee
drat3 <drat3@protonmail.ch>
2023-12-24 13:19:52 +0100

    listen for twilio formdata and added crude logs

--- services/api/main.py
+++ services/api/main.py
@@ -1,13 +1,24 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import logging
+
+logging.basicConfig(filename='/home/app/logs/print.log', level=logging.INFO, format='%(asctime)s - %(message)s')
 
 app = FastAPI()
 
 
-@app.get("/")
-async def hello():
-    return "Hello World"
+@app.get("/ping")
+async def ping():
+    return "pong"
 
+@app.post("/incoming-sms-hook")
+async def receive_form_data(request: Request):
+    form_data = await request.form()
+    data_dict = dict(form_data)
+
+    logging.info(data_dict)
+
+    return "Ok"
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)

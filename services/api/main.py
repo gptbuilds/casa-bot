dcf8aa739cd9906a1eb39eea81282a9cefa6df83
drat3 <drat3@protonmail.ch>
2023-12-25 16:47:47 +0100

    Untested implementation of agent

--- services/api/main.py
+++ services/api/main.py
@@ -1,23 +1,30 @@
+import fastapi
+import os
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from pydantic import BaseModel, Field
-
 import logging
+from langchain import MongoDBChatMessageHistory, ConversationBufferMemory, OpenAI, ConversationChain, PromptTemplate
 
 logging.basicConfig(filename='/home/app/logs/print.log', level=logging.INFO, format='%(asctime)s - %(message)s')
 
 app = FastAPI()
+MONGO_CONN = os.getenv("MONGO_CONNECTION_STRING")
 
 class Message(BaseModel):
     phone_number: str 
     text_message: str
+
+class TestWrap:
+    message: Message
+    password: str
 
 @app.get("/ping")
 async def ping():
     return "pong"
 
 @app.post("/incoming-sms-hook")
-async def receive_form_data(request: Request):
+async def incoming_sms_hook(request: Request):
     form_data = await request.form()
     data_dict = dict(form_data)
 
@@ -27,9 +34,44 @@ async def receive_form_data(request: Request):
 
 # This will be removed once we go live
 @app.post("/only-for-testing-agent")
-async def only_for_testing_agent(message: Message):
-    #RpfokVcXmxzAedtu
-    return "Ok"
+async def only_for_testing_agent(wrap: TestWrap) -> str:
+    if wrap.password == "BadMotherfucker":
+        return await execute_message(wrap.message);
+    else:
+        raise HTTPException(status_code=fastapi.status.HTTP_403_FORBIDDEN, detail="Access forbidden")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+async def execute_message(message: Message) -> str:
+    message_history = MongoDBChatMessageHistory(
+        connection_string=MONGO_CONN, session_id= message.phone_number
+    )
+
+    memory = ConversationBufferMemory()
+
+    for i in range(0, len(message_history.messages), 2):
+        if i + 1 < len(message_history.messages):
+            memory.chat_memory.add_user_message(message_history.messages[0].content)
+            memory.chat_memory.add_user_message(message_history.messages[i + 1].content)
+
+    llm = OpenAI(temperature=0.5)
+    template = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+
+Current conversation:
+{history}
+Human: {input}
+AI Assistant:"""
+    PROMPT = PromptTemplate(input_variables=["history", "input"], template=template)
+    conversation = ConversationChain(
+                                llm=llm,
+                                verbose=False,
+                                prompt = PROMPT,
+                                memory=memory
+                                )
+    ## get the result
+    conv = conversation.predict(input=message.text_message , link=message.phone_number )
+    
+    return conv
+    
+
